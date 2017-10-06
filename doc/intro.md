996d891b6aa5a41aa478a4e273567816910fda2a
carcigenicate <slomifier@gmail.com>
2017-10-06 15:02:37 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mine-sweeper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/mine_sweeper/core_test.clj
@@ -0,0 +1,7 @@
+(ns mine-sweeper.core-test
+  (:require [clojure.test :refer :all]
+            [mine-sweeper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
