558d10fb1ebba0e870ec3d89145ca014d27a9163
carcigenicate <slomifier@gmail.com>
2017-10-10 12:52:19 -0600

    FINALLY GOT THE FLOOD FILL TO WORK!

--- src/mine_sweeper/flood_fill.clj
+++ src/mine_sweeper/flood_fill.clj
@@ -2,7 +2,7 @@
   (:require [mine-sweeper.board :as b]
             [mine-sweeper.tiles :as t]))
 
-(defn coords-surrounding [board center-x center-y]
+(defn floodable-coords-surrounding [board center-x center-y]
   (let [[w h] (:dimensions board)]
     (for [y (range (dec center-y) (+ center-y 2))
           x (range (dec center-x) (+ center-x 2))
@@ -10,27 +10,75 @@
                      (b/inbounds? board x y))]
       [x y])))
 
+(defn remove-terminating-coords [board visited pred coords]
+  (remove (fn [[x y :as p]]
+            (or (visited p)
+                (not (pred (b/get-cell board x y)))))
+
+          coords))
+#_
 (defn flood-fill-while [board x y fill-with pred]
   (loop [acc-board board
          frontier [[x y]]
-         visited #{[x y]}]
+         visited #{}]
+
+    (println "Popped:" (first frontier))
+    (println "V:" visited)
+    (println "FSe:" (into #{} frontier) "\n")
 
     (if-not (empty? frontier)
       (let [[[px py :as popped] & rest-frontier] frontier
-            frontier-additions (coords-surrounding acc-board px py)
+            updated-visited (conj visited popped)
+            frontier-additions (floodable-coords-surrounding acc-board px py)
 
             ; FIXME: Dangerously lazy?
-            new-frontier (remove visited (concat rest-frontier frontier-additions))]
+            new-frontier (vec
+                           (remove-terminating-coords updated-visited pred
+                             (into frontier-additions rest-frontier)))]
 
         (recur
-          (if (and (pred (b/get-cell board px py)) (not (visited popped)))
+          (if (and (pred (b/get-cell acc-board px py)) (not (visited popped)))
             (b/set-cell acc-board px py fill-with)
             acc-board)
 
           new-frontier
           (conj visited popped)))
 
-      board)))
+      acc-board)))
+
+(defn flood-fill-while [board fill-x fill-y fill-contents pred]
+  (let [initial-fill [fill-x fill-y]]
+    (loop [frontier [initial-fill]
+           flooded #{}
+           acc-board board]
+
+      (if-not (empty? frontier)
+        (let [[[x y :as current-coord] & rest-frontier] frontier
+              inbound-neighbors (floodable-coords-surrounding acc-board x y)
+
+              self-flooded (conj flooded current-coord)
+
+              updated-frontier  (remove-terminating-coords acc-board
+                                                           self-flooded
+                                                           pred
+                                                           (into rest-frontier
+                                                                 inbound-neighbors))]
+
+          (recur
+            updated-frontier
+            self-flooded
+            (if (pred (b/get-cell acc-board x y))
+              (b/set-cell acc-board x y fill-contents)
+              acc-board)))
+
+        acc-board))))
+
+
+
+
+
+
+
 
 
 
--- src/mine_sweeper/game_board.clj
+++ src/mine_sweeper/game_board.clj
@@ -1,7 +1,26 @@
 (ns mine-sweeper.game-board
   (:require [mine-sweeper.board :as b]
-            [mine-sweeper.tiles :as t]))
+            [mine-sweeper.tiles :as t]
+            [mine-sweeper.flood-fill :as ff]))
 
 (defn new-game-board [width height]
   (b/new-board width height ::t/covered))
 
+(defn draw-hor-divider [board x divider-contents]
+  (reduce #(b/set-cell % x %2 divider-contents)
+          board
+          (-> board :dimensions (second) (range))))
+
+(defn draw-vert-divider [board y divider-contents]
+  (reduce #(b/set-cell % %2 y divider-contents)
+          board
+          (-> board :dimensions (first) (range))))
+
+(def divided-test-board
+  (-> (b/new-board 7 3 :-)
+      (draw-hor-divider 2 :#)))
+
+(def bigger-divided-test-board
+  (-> (b/new-board 7 7 :-)
+      (draw-hor-divider 2 :#)
+      (draw-vert-divider 4 :#)))
