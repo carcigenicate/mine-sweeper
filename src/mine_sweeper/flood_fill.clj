0591d325f6f2f8aaf718528651677d6446cb1632
carcigenicate <slomifier@gmail.com>
2017-10-10 13:17:16 -0600

    Fixed up a bit. Bloated it, but fixed an issue where filling on a divider cell would fill the areas on its sides.

--- src/mine_sweeper/flood_fill.clj
+++ src/mine_sweeper/flood_fill.clj
@@ -9,69 +9,44 @@
           :when (and (not= [x y] [center-x center-y])
                      (b/inbounds? board x y))]
       [x y])))
-
+#_ ; Unnecessary? Too many parameters?
 (defn remove-terminating-coords [board visited pred coords]
   (remove (fn [[x y :as p]]
             (or (visited p)
                 (not (pred (b/get-cell board x y)))))
 
           coords))
-#_
-(defn flood-fill-while [board x y fill-with pred]
-  (loop [acc-board board
-         frontier [[x y]]
-         visited #{}]
-
-    (println "Popped:" (first frontier))
-    (println "V:" visited)
-    (println "FSe:" (into #{} frontier) "\n")
-
-    (if-not (empty? frontier)
-      (let [[[px py :as popped] & rest-frontier] frontier
-            updated-visited (conj visited popped)
-            frontier-additions (floodable-coords-surrounding acc-board px py)
-
-            ; FIXME: Dangerously lazy?
-            new-frontier (vec
-                           (remove-terminating-coords updated-visited pred
-                             (into frontier-additions rest-frontier)))]
-
-        (recur
-          (if (and (pred (b/get-cell acc-board px py)) (not (visited popped)))
-            (b/set-cell acc-board px py fill-with)
-            acc-board)
-
-          new-frontier
-          (conj visited popped)))
-
-      acc-board)))
-
+; TODO: Breakup
 (defn flood-fill-while [board fill-x fill-y fill-contents pred]
-  (let [initial-fill [fill-x fill-y]]
-    (loop [frontier [initial-fill]
-           flooded #{}
-           acc-board board]
+  (let [initial-fill [fill-x fill-y]
+        pred' (fn [b [cx cy]] (pred (b/get-cell b cx cy)))]
 
-      (if-not (empty? frontier)
-        (let [[[x y :as current-coord] & rest-frontier] frontier
-              inbound-neighbors (floodable-coords-surrounding acc-board x y)
+    (if (pred' board initial-fill)
+      (loop [frontier [initial-fill]
+             flooded #{}
+             acc-board board]
 
-              self-flooded (conj flooded current-coord)
+        (if-not (empty? frontier)
+          (let [pred'' (partial pred' acc-board)
+                [[x y :as current-coord] & rest-frontier] frontier
 
-              updated-frontier  (remove-terminating-coords acc-board
-                                                           self-flooded
-                                                           pred
-                                                           (into rest-frontier
-                                                                 inbound-neighbors))]
+                inbound-neighbors (floodable-coords-surrounding acc-board x y)
 
-          (recur
-            updated-frontier
-            self-flooded
-            (if (pred (b/get-cell acc-board x y))
-              (b/set-cell acc-board x y fill-contents)
-              acc-board)))
+                self-flooded (conj flooded current-coord)
 
-        acc-board))))
+                no-repeat-frontier (remove self-flooded (into rest-frontier inbound-neighbors))
+                no-divider-frontier (filter pred'' no-repeat-frontier)]
+
+            (recur
+              no-divider-frontier
+              self-flooded
+              (if (pred'' [x y])
+                (b/set-cell acc-board x y fill-contents)
+                acc-board)))
+
+          acc-board))
+
+      board)))
 
 
 
